0dae8b292e04c605a1dd822c9db325dcd403d6fb
Masonimo <masondpeterson@gmail.com>
2016-05-12 16:10:35 -0600

    Angular Version
    This is an attempt to reproduce the monsterAdditions using angular
    instead of bootstrap.

---
+++ addMon.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>New Monster</title>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+</head>
+<body>
+
+<div ng-app="firstExpression">
+ 	<p>My first expression: {{ 5 + 5 }}</p>
+</div>
+
+</body>
+</html>
---
+++ addMon.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>New Monster</title>
+<script src="addmon.js"></script>
+</head>
+<body>
+
+<div ng-app="firstExpression"></div>
+
+</body>
+</html>
